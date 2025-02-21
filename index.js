c4f56e8ca9c3de3a2b946bde95f4c633d78bd3a4
tm-fuad05 <tm.fuad05@gmail.com>
2025-02-21 10:44:22 +0600

    task api create

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ async function run() {
   try {
     // Database & Collection
     const database = client.db("TaskManagerDB");
-    const collection = database.collection("collection");
+    const userCollection = database.collection("users");
+    const taskCollection = database.collection("tasks");
 
     // Middleware
     const verifyToken = (req, res, next) => {
@@ -63,6 +64,95 @@ async function run() {
           token,
         });
       } catch (error) {
+        return res
+          .status(500)
+          .json({ success: false, message: "Internal server error." });
+      }
+    });
+
+    // User Api
+    app.post("/users", async (req, res) => {
+      try {
+        const userInfo = req.body;
+        if (!userInfo?.email) {
+          return res
+            .status(400)
+            .json({ success: false, message: "Invalid email" });
+        }
+
+        const query = { email: userInfo?.email };
+
+        const existingUser = await userCollection.findOne(query);
+        if (existingUser) {
+          return res
+            .status(409)
+            .json({ success: false, message: "User already exists." });
+        }
+
+        const result = await userCollection.insertOne(userInfo);
+
+        if (result.insertedId) {
+          return res.status(201).json({ success: true });
+        }
+        return res.status(500).json({
+          success: false,
+          message: "Failed to add the user.",
+        });
+      } catch (error) {
+        console.error(error);
+        return res
+          .status(500)
+          .json({ success: false, message: "Internal server error." });
+      }
+    });
+
+    app.get("/users", verifyToken, async (req, res) => {
+      try {
+        const result = await userCollection.find().toArray();
+
+        return res.status(200).json(result);
+      } catch (error) {
+        console.error(error);
+        return res
+          .status(500)
+          .json({ success: false, message: "Internal server error." });
+      }
+    });
+
+    // TaskApi
+    app.post("/tasks", async (req, res) => {
+      try {
+        const taskData = req.body;
+
+        if (!taskData || Object.keys(taskData).length === 0) {
+          return res
+            .status(400)
+            .json({ success: false, message: "TaskData data is required." });
+        }
+
+        const result = await taskCollection.insertOne(taskData);
+
+        if (result.insertedId) {
+          return res.status(201).json({ success: true });
+        }
+        return res
+          .status(500)
+          .json({ success: false, message: "Internal server error." });
+      } catch (error) {
+        console.error(error);
+        return res
+          .status(500)
+          .json({ success: false, message: "Internal server error." });
+      }
+    });
+
+    app.get("/tasks", async (req, res) => {
+      try {
+        const result = await taskCollection.find().toArray();
+
+        return res.status(200).json(result);
+      } catch (error) {
+        console.error(error);
         return res
           .status(500)
           .json({ success: false, message: "Internal server error." });
